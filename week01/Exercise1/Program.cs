23bcdee56cab1b1b66c370a3fee641ea26bb56e7
JamesOnGitHub54 <jamesg521@outlook.com>
2025-06-14 21:40:04 -0600

    Completed Exercise 1.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Get user's full name, separated by first and last name.
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        // Display user's full name in as James Bond would.
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
