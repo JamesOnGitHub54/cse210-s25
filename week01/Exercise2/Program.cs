512cb23f1e022c4aa30be14bca73fb0e1b3918fe
JamesOnGitHub54 <jamesg521@outlook.com>
2025-06-14 21:54:24 -0600

    Partial completion of Core requirements of Exercise 2.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        int grade_percent = int.Parse(Console.ReadLine());
+        string letter_grade;
+
+        if (grade_percent >= 90)
+        {
+            letter_grade = "A";
+        }
+        else if (grade_percent >= 80)
+        {
+            letter_grade = "B";
+        }
+        else if (grade_percent >= 70)
+        {
+            letter_grade = "C";
+        }
+        else if (grade_percent >= 60)
+        {
+            letter_grade = "D";
+        }
+        else letter_grade = "F";
+
+        Console.WriteLine($"You have earned the letter grade: {letter_grade}");
+
     }
 }
