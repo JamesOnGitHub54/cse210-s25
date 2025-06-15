2395b4c05f09768f4bbbd669b2adb03689ae5300
JamesOnGitHub54 <jamesg521@outlook.com>
2025-06-14 22:56:51 -0600

    Completed stretch challenge in Exercise 3.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,33 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        // User creates the magic number
-        // Console.Write("What is the magic number?: ");
-        // int magic_number = int.Parse(Console.ReadLine());
-
-        // Magic number is randomly generated.
-        Random randomGenerator = new Random();
-        int magic_number = randomGenerator.Next(1, 101);
-
-        // +1 allows to establish int guess while not matching magic number at startup.
-        int guess = magic_number + 1;
-
-        // Loop into guessing until magic number is found. Hints are given for each guess.
-        while (guess != magic_number)
+        // Establish replay as yes. Program will end when user does not enter "yes".
+        string replay = "yes";
+        while (replay == "yes")
         {
-            Console.Write("What is your guess?: ");
-            guess = int.Parse(Console.ReadLine());
-            if (guess > magic_number)
-            {
-                Console.WriteLine("Lower");
-            }
-            else if (guess < magic_number)
-            {
-                Console.WriteLine("Higher");
-            }
-            else Console.WriteLine("You guessed it!");
+            // User creates the magic number
+            // Console.Write("What is the magic number?: ");
+            // int magic_number = int.Parse(Console.ReadLine());
 
+            // Magic number is randomly generated.
+            Random randomGenerator = new Random();
+            int magic_number = randomGenerator.Next(1, 101);
 
+            // +1 allows to establish int guess while not matching magic number at startup.
+            int guess = magic_number + 1;
+            int attempt = 0;
+
+            // Loop into guessing until magic number is found. Hints are given for each guess.
+            while (guess != magic_number)
+            {
+                attempt = attempt + 1;
+                Console.Write("What is your guess?: ");
+                guess = int.Parse(Console.ReadLine());
+                if (guess > magic_number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magic_number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else Console.WriteLine($"You guessed it! Number of attempts: {attempt}");
+            }
+
+            // User may play again if yes is entered. New magic number is generated and attempts reset.
+            Console.Write("Would you like to play again?: ");
+            replay = Console.ReadLine();
         }
+
+        Console.WriteLine("Thank you for playing!");
     }
 }
