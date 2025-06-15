60bc592c3584eb0334034cc13d82bfd8749f23ce
JamesOnGitHub54 <jamesg521@outlook.com>
2025-06-14 22:41:17 -0600

    Completed Exercise 3.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // User creates the magic number
+        // Console.Write("What is the magic number?: ");
+        // int magic_number = int.Parse(Console.ReadLine());
+
+        // Magic number is randomly generated.
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 101);
+
+        // +1 allows to establish int guess while not matching magic number at startup.
+        int guess = magic_number + 1;
+
+        // Loop into guessing until magic number is found. Hints are given for each guess.
+        while (guess != magic_number)
+        {
+            Console.Write("What is your guess?: ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else Console.WriteLine("You guessed it!");
+
+
+        }
     }
 }
