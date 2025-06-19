0a4664c317dad32bad97ca831811b75b2a477149
JamesOnGitHub54 <jamesg521@outlook.com>
2025-06-19 17:39:31 -0600

    Completed Exercise 5 before implementing functions.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // Welcome message.
+        Console.WriteLine("Welcome to the program!");
+
+        // Get user's name.
+        Console.Write("Please enter your name: ");
+        string user_name = Console.ReadLine();
+
+        // Get user number.
+        Console.Write("Please enter your favorite number: ");
+        int favorite_number = int.Parse(Console.ReadLine());
+
+        // Get square of number.
+        int square = favorite_number * favorite_number;
+
+        // Display results.
+        Console.WriteLine($"{user_name}, the square of your number is {square}");
+
+
     }
 }
