5d86be846bb76e152aaa34b36623c6dac0829048
JamesOnGitHub54 <jamesg521@outlook.com>
2025-06-19 16:58:07 -0600

    Basic start to Exercise 4.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        /// Set user number and give program instructions.
+        int user_number = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        /// Continue to enter numbers until 0 is typed.
+        while (user_number != 0)
+        {
+            Console.WriteLine("Enter number: ");
+            user_number = int.Parse(Console.ReadLine());
+        }
     }
 }
