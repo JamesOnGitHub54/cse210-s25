0e496d3673478637ead604d40b39a8f36582a78f
JamesOnGitHub54 <jamesg521@outlook.com>
2025-06-19 18:29:15 -0600

    Completed step 5 of resume assignment.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,14 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Creating job 1.
         Job job1 = new Job();
         job1._jobTitle = "Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = "2017";
+        job1._endYear = "2019";
 
+        // Creating job 2.
         Job job2 = new Job();
-        job2._company = "Microsoft";
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = "2020";
+        job2._endYear = "2022";
 
-        Console.WriteLine(job1._jobTitle);
-        Console.WriteLine(job2._company);
+        // Display jobs.
+        job1.Display();
+        job2.Display();
+
+
 
 
     }
