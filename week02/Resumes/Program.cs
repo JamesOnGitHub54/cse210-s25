80f9c71ecf920cfc0cc6ecfc0d5808d731ce5e63
JamesOnGitHub54 <jamesg521@outlook.com>
2025-06-19 18:13:56 -0600

    Progress made on Resumes. Completed steps 1-4.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Engineer";
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+
+        Console.WriteLine(job1._jobTitle);
+        Console.WriteLine(job2._company);
+
+
     }
 }
