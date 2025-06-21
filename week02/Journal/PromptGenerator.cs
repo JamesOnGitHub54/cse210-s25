2a76101dd3d56fac71521bd8bdaf4c092c536377
JamesOnGitHub54 <jamesg521@outlook.com>
2025-06-21 15:41:43 -0600

    Save Point 2 after watching start video for Journal program.

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -4,18 +4,23 @@ public class PromptGenerator
 {
     public List<string> _prompts = new List<string>();
 
+    public string stringGetRandomPrompt()
+    {
+        return "";
+    }
+
     // Manually adding prompts to the program.
-    public PromptGenerator()
+    /**public PromptGenerator()
     {
         _prompts.Add("What did you do today?");
         _prompts.Add("Who did you meet with today?");
         _prompts.Add("If I had one thing I could do over today, what would it be?");
         _prompts.Add("What was the best part of my day?");
         _prompts.Add("Who was the most interesting person I interacted with today?");
-    }
+    }**/
 
-    
-    
+
+
 
 
 
