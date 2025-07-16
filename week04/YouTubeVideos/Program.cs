3214dd3467a7ecc645f235c25f6f1f148de7e8df
JamesOnGitHub54 <jamesg521@outlook.com>
2025-07-15 21:05:22 -0600

    Created Method, modified classes, and created first video. Work in progress.

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        // Create Video 1.
+        Video video1 = new Video();
+        video1._title = "lofi hip hop radio - beats to relax/study to";
+        video1._author = "Lofi Girl";
+        video1._length = "75,037,431";
+        
+        
     }
 }
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -3,7 +3,21 @@ public class Video
     public string _title;
     public string _author;
     public string _length;
+    public string _comment_number;
     public List<Comment> _comments = new List<Comment>();
+
+// Gets the number of comments for a video
+    public string Number()
+    {
+        // Iterate through each comment, adding to the total number of comments.
+        foreach (Comment comment in _comments)
+        {
+            _comment_number += 1;
+        }
+        // Learned Convert.ToString() via W3Schools: https://www.w3schools.com/cs/cs_type_casting.php
+        Convert.ToString(_comment_number);
+        return _comment_number;
+    }
 
     public void Display()
     {
