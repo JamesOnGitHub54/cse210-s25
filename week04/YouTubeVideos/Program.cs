c17657d0fc8b070fad2bbed940df3f0eb343fba1
JamesOnGitHub54 <jamesg521@outlook.com>
2025-07-15 22:03:30 -0600

    Program format made. Make list and iterate step to follow.

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
@@ -29,10 +30,62 @@ class Program
         video1._comments.Add(comment2);
         video1._comments.Add(comment3);
 
+        // Create Comment 4.
+        Comment comment4 = new Comment();
+        comment4._commenter = "@EIMINE";
+        comment4._comment = "Finally lofi girl is ready for her exams";
+        // Create Comment 5.
+        Comment comment5 = new Comment();
+        comment5._commenter = "@EIMINE";
+        comment5._comment = "Finally lofi girl is ready for her exams";
+        // Create Comment 6.
+        Comment comment6 = new Comment();
+        comment6._commenter = "@EIMINE";
+        comment6._comment = "Finally lofi girl is ready for her exams";
+
+        // Create Video 2.
+        Video video2 = new Video();
+        video2._title = "lofi hip hop radio - beats to relax/study to";
+        video2._author = "@LofiGirl";
+        video2._length = "75,037,431";
+        video2._comments.Add(comment4);
+        video2._comments.Add(comment5);
+        video2._comments.Add(comment6);
+
+        // Create Comment 7.
+        Comment comment7 = new Comment();
+        comment7._commenter = "@EIMINE";
+        comment7._comment = "Finally lofi girl is ready for her exams";
+        // Create Comment 8.
+        Comment comment8 = new Comment();
+        comment8._commenter = "@EIMINE";
+        comment8._comment = "Finally lofi girl is ready for her exams";
+        // Create Comment 9.
+        Comment comment9 = new Comment();
+        comment9._commenter = "@EIMINE";
+        comment9._comment = "Finally lofi girl is ready for her exams";
+
+        // Create Video 3.
+        Video video3 = new Video();
+        video3._title = "lofi hip hop radio - beats to relax/study to";
+        video3._author = "@LofiGirl";
+        video3._length = "75,037,431";
+        video3._comments.Add(comment7);
+        video3._comments.Add(comment8);
+        video3._comments.Add(comment9);
+
+
+
         // Check number of comments
         video1.Number();
+        video2.Number();
+        video3.Number();
 
         // Display all.
         video1.Display();
+        Console.WriteLine();
+        video2.Display();
+        Console.WriteLine();
+        video3.Display();
     }
 }
