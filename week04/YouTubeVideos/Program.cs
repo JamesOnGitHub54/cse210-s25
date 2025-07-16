bfd398ce8fcf2c666c84a4e80456fecc79c8cbde
JamesOnGitHub54 <jamesg521@outlook.com>
2025-07-15 21:20:28 -0600

    First video with comments is created. Method is not functioning as expected.

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,12 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        // Create Comment 1.
+        Comment comment1 = new Comment();
+        comment1._commenter = "@apple5293";
+        comment1._comment = "i never thought this would end";
+
+        // Create Comment 2.
+        Comment comment2 = new Comment();
+        comment2._commenter = "@Veshiram";
+        comment2._comment = "As Lofi Girl's teacher, I'm proud of seeing her study for 2 years. She aced the final test.";
+
+        // Create Comment 3.
+        Comment comment3 = new Comment();
+        comment3._commenter = "@EIMINE";
+        comment3._comment = "Finally lofi girl is ready for her exams";
+
         // Create Video 1.
         Video video1 = new Video();
         video1._title = "lofi hip hop radio - beats to relax/study to";
-        video1._author = "Lofi Girl";
+        video1._author = "@LofiGirl";
         video1._length = "75,037,431";
-        
-        
+        video1._comments.Add(comment1);
+        video1._comments.Add(comment2);
+        video1._comments.Add(comment3);
+
+        // Check number of comments
+        video1.Number();
+
+        // Display all.
+        video1.Display();
     }
 }
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -22,7 +22,7 @@ public class Video
     public void Display()
     {
         // Present core video details.
-        Console.WriteLine($"Title: {_title}\nAuthor: {_author}\nVideo length: {_length} seconds");
+        Console.WriteLine($"Title: {_title}\nAuthor: {_author}\nVideo length: {_length} seconds\nNumber of comments: {_comment_number}");
         // Iterate through each comment, formatted in the Comment file.
         foreach (Comment comment in _comments)
         {
